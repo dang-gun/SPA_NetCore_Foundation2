b137eb73a3d4a93e7a10e1471cccc03f800a16d3
dang-gun <c1dong8@gmail.com>
2022-09-05 19:15:18 +0900

    AspNetCore6React : DB, 리액트 세팅 완료

---
+++ AspNetCore6React/Global/GlobalStatic.cs
@@ -0,0 +1,29 @@
+﻿using ModelsDB;
+
+namespace AspNetCore6React.Global;
+
+public static class GlobalStatic
+{
+	/// <summary>
+	/// DB 타입
+	/// </summary>
+	/// <remarks>저장전에 소문자로 변환해야 한다.</remarks>
+	public static string DBType = "";
+	/// <summary>
+	/// DB 컨낵션 스트링 저장
+	/// </summary>
+	public static string DBString = "";
+
+	/// <summary>
+	/// 사인인 가능한 유저 정보
+	/// </summary>
+	public static List<User> Users;
+
+	static GlobalStatic()
+	{
+		Users = new List<User>();
+		Users.Add(new User() { idUser = 1, SignName = "test01", PasswordHash = "1111" });
+		Users.Add(new User() { idUser = 2, SignName = "test02", PasswordHash = "1111" });
+		Users.Add(new User() { idUser = 3, SignName = "test03", PasswordHash = "1111" });
+	}
+}
--- AspNetCoreNpmApi/src/sass/postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ options }) => {
-  const plugins = [];
-  if (options.env === 'production') plugins.push(require('cssnano'));
-  return {
-      plugins: plugins
-  };
-};
